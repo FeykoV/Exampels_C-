f11bc41f5d2612a4d85016728b7701a39997f483
Елена <volshebnaya2393@gmail.com>
2023-01-22 20:08:09 +0500

    работа с массивами

---
+++ Ex010/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 12, 4, 54, 5, 4, 57, 8};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;  // оператор прерывания работы программы
+    }
+    index++;
+}
