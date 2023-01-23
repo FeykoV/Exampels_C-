94f23d844f7dc735cd0941d5d834ba61a0470a70
Елена <volshebnaya2393@gmail.com>
2023-01-23 15:41:37 +0500

    Оператор for

---
+++ Ex012_2/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty; //где String.Empty = ""  - это значит пустая строка.
+    for(int i =0; i < count; i++)
+    {
+        result = result  + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "qwerty");
+Console.WriteLine(res);
