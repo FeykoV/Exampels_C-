2056a121955bd8e42ab6fb625210e45f4af94b09
Елена <volshebnaya2393@gmail.com>
2023-02-15 19:21:39 +0500

    Первая часть дополнительной лекции по рекурсии

---
+++ Ex_Dop3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Перебор слов
+
+char[] s = { 'а', 'и', 'с', 'в'};
+int count = s.Length;
+int n = 1;
+for (int i = 0; i < count; i++)
+{
+    for (int j = 0; j < count; j++)
+    {
+        for (int k = 0; k < count; k++)
+        {
+            for (int l = 0; l < count; l++)
+            {
+                Console.WriteLine($"{n++, -5}{s[i]}{s[j]}{s[k]}{s[l]}");
+            }
+        }
+    }
+}
+
+// а теперь эту задачу решим рекурсией
+
+void FindWords(string alphabet, char [] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}");
+        return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+
+FindWords("йцук", new char[5]);
